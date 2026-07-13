1e45872ff7956d5295cfdf2b23176a4614f10f91
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2026-07-13 12:11:34 +0200

    ci(workflows): run app test only for chromium, disable gh coverage, add codecov coverage
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- apps/app/playwright.config.ts
+++ apps/app/playwright.config.ts
@@ -7,20 +7,27 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   fullyParallel: true,
   globalSetup: "./tests/global-setup.ts",
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
+  projects: process.env.CI
+    ? [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+      ]
+    : [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+        {
+          name: "firefox",
+          use: { ...devices["Desktop Firefox"] },
+        },
+        {
+          name: "webkit",
+          use: { ...devices["Desktop Safari"] },
+        },
+      ],
   reporter: "html",
   retries: process.env.CI ? 2 : 0,
   testDir: "./tests",
