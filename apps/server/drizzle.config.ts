7406c501ec2cbe4499eb29625721e5a2bd1b9714
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2026-07-13 10:16:37 +0200

    fix(server): drizzle schema path
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "sqlite",
   out: "./drizzle",
-  schema: "./src/lib/db/schemas/index.ts",
+  schema: "./src/db/schemas/index.ts",
 });
